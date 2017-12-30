d40bdb2b1581697381cceeb65b4582fe6a793213
lannle <le_l1@denison.edu>
2017-12-29 23:29:55 -0800

    Handled null object

--- server.js
+++ server.js
@@ -14,27 +14,25 @@ app.post('/sms', (req, res) => {
 
   var dealRequest = req.body.Body
   Deal.findDeal(dealRequest).then((result) => {
+  		
   		const twiml = new MessagingResponse();
-	    var newDeal = result.deal;
-	  	twiml.message(newDeal);
+	    var newDeal = result;
+
+	    if (newDeal ==null) {
+  			if(req.body.Body=="STORE"){
+	  			twiml.message('Avaiable Store: SHEIN,Charlotte Russe, Forever 21, Sephora, Macys, Target. Text the name of the store to get the current deal!')
+	  		}else {
+	    		twiml.message('Want to get deals from your favorite store? Text STORE to check all the stores in our service');
+	  		}  
+  		} else {
+  			twiml.message(newDeal.deal);
+  		}	  	
 
 	  	res.writeHead(200, {'Content-Type': 'text/xml'});
   		res.end(twiml.toString()); 
 
 	  }).catch((err) => {
 	  		throw err;
-	  		twiml.message('Want to get deals from your favorite store? Text STORE to check all the stores in our service');
-		    // console.log(err)
-		    // const twiml = new MessagingResponse();
-		   //  if (newDeal == null){
-		   //  	if(req.body.Body=="STORE"){
-	  		// 		twiml.message('Avaiable Store: SHEIN,Charlotte Russe, Forever 21, Sephora, Macys, Target. Text the name of the store to get the current deal!')
-	  		// 	}else {
-	    // 			twiml.message('Want to get deals from your favorite store? Text STORE to check all the stores in our service');
-	  		// 	}  
-		   //  }
-		    res.writeHead(200, {'Content-Type': 'text/xml'});
-  			res.end(twiml.toString()); 
 		    
 	  })
   
