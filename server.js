6f3c6841b35fdd46f58f4568ec64b2f673d4b317
lannle <le_l1@denison.edu>
2018-01-08 17:43:01 -0800

    update

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const http = require('http');
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const twilioClient = require('twilio')(process.env.TWILIO_ACCOUNTSID, process.env.TWILIO_AUTHTOKEN);
+const ClientCapability = require('twilio').jwt.ClientCapability;
 const bodyParser = require('body-parser');
 
 var Deal = require('./models/deal');
@@ -14,6 +15,23 @@ const app = express();
 
 app.use(bodyParser());
 
+
+app.get('/token', (req, res) => {
+
+  const capability = new ClientCapability({
+    accountSid: process.env.TWILIO_ACCOUNTSID,
+    authToken: process.env.TWILIO_AUTHTOKEN,
+  });
+  capability.addScope(
+    new ClientCapability.OutgoingClientScope({ applicationSid: process.env.TWILIO_APPSID })
+  );
+  capability.addScope(new ClientCapability.IncomingClientScope('joey'));
+  const token = capability.toJwt();
+
+  res.set('Content-Type', 'application/jwt');
+  res.send(token);
+});
+
 app.post('/sms', (req, res) => {
   var dealRequest = req.body.Body.trim();
   Deal.findDeal(dealRequest).then((result) => {
@@ -21,29 +39,19 @@ app.post('/sms', (req, res) => {
   		const twiml = new MessagingResponse();
 	    var newDeal = result;
 
-	    if (newDeal ==null) {
-  			if(req.body.Body=="STORE"){
-          twilioClient.messages.create({
-            to: req.body.From,
-            from: req.body.To,
-            body: 'Avaiable Stores: SHEIN,Charlotte Russe, Forever 21, Sephora, Macys, Target. Text the name of the store to get the current deal!'
-          })
-	  		}else {
-          twilioClient.messages.create({
-            to: req.body.From,
-            from: req.body.To,
-            body: 'Want to get deals from your favorite stores? Text STORE to check all the stores in our service'
-          })
-	  		}  
-  		} else {
-  			//write a function to remove unwanted string 
-  			var msg = convertCharacters(newDeal.deal);
-        twilioClient.messages.create({
-            to: req.body.From,
-            from: req.body.To,
-            body: msg
-          })
-  		}	  	
+      if (newDeal ==null) {
+        if(req.body.Body=="STORE"){
+          twiml.message('Avaiable Stores: SHEIN,Charlotte Russe, Forever 21, Sephora, Macys, Target. Text the name of the store to get the current deal!')
+        }else {
+          twiml.message('Want to get deals from your favorite stores? Text STORE to check all the stores in our service');
+        }  
+      } else {
+        //write a function to remove unwanted string 
+
+        var msg = convertCharacters(newDeal.deal);
+        twiml.message(msg);
+
+      }     	  	
       res.set('Content-Type', 'text/xml');
       res.status(200).send(twiml.toString());
 
